d8ca545c828ff09216f97379e3f48ff97da290f0
diaslilian <lilian.dias25@gmail.com>
2020-07-30 12:35:58 -0300

    eslint-disable / accessible-emoji

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
 
 import { Container, Retweeted, RocketseatIcon, Body, Avatar, Content, Header, Dot, Description, ImageContent, Icons, Status, CommentIcon, RetweetIcon, LikeIcon } from './styles';
